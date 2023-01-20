bc5eceb93518a7b75f46df3b76e1cf20c2bb837a
Welligton Porto <welligtonporto@gmail.com>
2023-01-20 11:47:40 -0300

    Cleaning master

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/hello", (req, res) => {
-    res.send("Hello world!");
-});
-
-const PORT = 3000;
-app.listen(PORT, console.log(`Running on http://localhost:${PORT}`));
